7d43186ea395d21f7792580e4f249d2f869e8447
mythily <58061358+mythilys@users.noreply.github.com>
2020-11-30 10:06:10 +0530

    Create randomintgenerator.go

---
+++ Assignment-1/randomintgenerator.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	fmt.Println(rand.Intn(100))
+	fmt.Println(rand.Intn(100))
+	fmt.Println(rand.Intn(100))
+	fmt.Println(rand.Intn(100))
+	fmt.Println(rand.Intn(100))
+	fmt.Println(rand.Intn(100))
+	fmt.Println(rand.Intn(100))
+}
